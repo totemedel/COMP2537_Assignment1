4350f34b0c5af2192a95c0cf9a77607b1f7ba762
Tony Chen <xchen232@my.bcit.ca>
2025-05-05 23:07:27 -0700

    added checklist

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("./utils.js");
 require('dotenv').config();
 const express = require('express');
+
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
